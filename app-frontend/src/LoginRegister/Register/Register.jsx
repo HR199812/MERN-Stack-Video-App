f123d254ac30a3c1e64fefe4b0620bbed665263a
Hritwik Agarwal <39455725+HR199812@users.noreply.github.com>
2021-10-19 18:18:50 +0530

    Added phone number with country input

--- app-frontend/src/LoginRegister/Register/Register.jsx
+++ app-frontend/src/LoginRegister/Register/Register.jsx
@@ -4,6 +4,10 @@ import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 import '../Login/Login.css';
 // import 'react-toastify/dist/ReactToastify.css';
+import 'react-phone-number-input/style.css';
+import PhoneInput from 'react-phone-number-input';
+import parsePhoneNumber from 'libphonenumber-js'
+
 
 function Register() {
 
@@ -16,13 +20,17 @@ function Register() {
         password: '',
         confirmPassword: '',
         phonenumber: '',
-    })
+    });
+
+    const [phoneNumber, setPhoneNumber] = useState('');
+
     const ShowLoginPage = () => {
         history.push('/');
     }
 
     const Create = () => {
-
+        user.phonenumber = parsePhoneNumber(phoneNumber).formatInternational();
+        alert(user.phonenumber);
     }
 
     const inputValueChange = (e) => {
@@ -46,12 +54,18 @@ function Register() {
                             <input type="text" onChange={inputValueChange} value={user.username} name="username" placeholder="User-Name" />
                             <input type="password" onChange={inputValueChange} value={user.password} name="password" placeholder="Password" />
                             <input type="password" onChange={inputValueChange} value={user.confirmPassword} name="confirmPassword" placeholder="Confirm-Password" />
+                            <PhoneInput
+                                placeholder="Enter phone number"
+                                defaultCountry="IN"
+                                value={phoneNumber}
+                                onChange={setPhoneNumber} />
+
                             <span>
-                                <select name="countrycode" id="countrycodeselect">
+                                {/* <select name="countrycode" id="countrycodeselect">
                                 </select>
-                                <input id="countrycodeinput" onChange={inputValueChange} value={user.phonenumber} name="phonenumber" type="phone" placeholder="Phone Number" />
+                                <input id="countrycodeinput" onChange={inputValueChange} value={user.phonenumber} name="phonenumber" type="phone" placeholder="Phone Number" /> */}
                             </span>
-                            <button onClick={Create}>create</button>
+                            <button onClick={() => Create}>create</button>
                             <p className="message">Already registered? <span onClick={ShowLoginPage}>Sign In</span></p>
                         </form>
                     </div>
