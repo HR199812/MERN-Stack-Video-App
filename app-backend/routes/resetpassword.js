939588418af62d3786160aee1d0da5c797a64681
Hritwik Agarwal <39455725+HR199812@users.noreply.github.com>
2021-10-21 15:30:15 +0530

    Added NodeMailer to send OTP to Mail

--- app-backend/routes/resetpassword.js
+++ app-backend/routes/resetpassword.js
@@ -18,7 +18,7 @@ router.post("/sendotp", (req, res) => {
         .save()
         .then((data) => {
           console.log("Check");
-          // sendMail(otpData);
+          sendMail(otpData);
           res.status(200).json("OTP send to Mail! Please Check your E-Mail");
         })
         .catch((err) => {
@@ -29,4 +29,44 @@ router.post("/sendotp", (req, res) => {
     }
   });
 });
+
+const sendMail = async (mailData) => {
+  console.log(mailData);
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  let testAccount = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: testAccount.user, // generated ethereal user
+      pass: testAccount.pass, // generated ethereal password
+    },
+  });
+
+  const mins = Math.floor(mailData.ExpireIn / 1000);
+  // var mins = moment.duration(x, 'milliseconds');
+  // moment(mins.asMinutes(), 'mm').format('mm:ss');
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: "hritwik72@gmail.com", // sender address
+    to: `${mailData.Email}`, // list of receivers
+    subject: "Reset Password", // Subject line
+    text: `Hi ${mailData.Email},
+        \nYour OTP to reset your password is: ${mailData.Code}.\n
+        Expiring in: 5 mins`, // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+};
 module.exports = router;
